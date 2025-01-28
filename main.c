fc8b59ab33c14d79dcdea507d1ab7ae352f69789
Xander Roy <jskpak0903@gmail.com>
2025-01-28 11:20:24 +0000

    this doesn't work

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello World");
+    char buffer[512];
+    while (1) {
+        printf(">"); //print input prompt
+        strcpy(buffer, ""); //clean buffer from any prev input
+
+        fgets(buffer, 511, stdin); //read input
+        if (!strcmp(buffer, "exit")) { //if exit, terminate
+            break;
+        }
+    }
+
+    return 0; //return no errors
 }
