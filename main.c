17272609ba7f27af7a13e11962e980b308a0f9ad
Xander Roy <jskpak0903@gmail.com>
2025-01-28 12:08:45 +0000

    changes to main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+char* parse(char* buffer);
+
 int main() {
     char buffer[512];
     while (1) {
@@ -8,10 +10,22 @@ int main() {
         strcpy(buffer, ""); //clean buffer from any prev input
 
         fgets(buffer, 511, stdin); //read input
-        if (!strcmp(buffer, "exit")) { //if exit, terminate
+
+        char* command = parse(buffer);
+
+        if (!strcmp(command, "exit")) { //if exit, terminate
             break;
         }
     }
 
     return 0; //return no errors
+}
+
+char* parse(char* buffer) {
+    char* tokens = strtok(buffer, " "); //creates a pointer to tokens[0][0]
+    printf(tokens);
+
+    /* TODO validate the input */
+
+    return tokens;
 }
